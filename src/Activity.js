e5ab2fc4302a5c44ae04707d988703e515e89fe4
Davidknp <dk@davidkonopka.de>
2022-05-29 21:58:19 +0200

    skeleton

---
+++ src/Activity.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Activity({activ}) {
+  return (
+    <div><h1>{ activ }</h1></div>
+  )
+}
--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Activity from "./Activity";
 
 function App() {
+  //const [items, setItems] = useState([]);
+
+  /*useEffect(() => {
+    fetch("https://www.boredapi.com/api/activity")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          setItems(result);
+        }
+      )
+  },[]);*/
+  const activ = 'Some other message';
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    
+    <div>
+      <h1>React-Bore-DOM</h1>
+      <p>Click the Button get a Random activity</p>
+      <Activity activ={ activ } />
+      <button>New Random activity</button>
     </div>
+    </>
   );
 }
 
